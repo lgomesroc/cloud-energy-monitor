12a7e1be670b3093abbd3b6bc4be1f2a1858bb9a
lgomesroc <lgomesroc2012@gmail.com>
2026-08-14 12:17:04 -0300

    refactor: type energy service result

--- src/services/energy.service.ts
+++ src/services/energy.service.ts
@@ -1,5 +1,6 @@
+import type { EnergyReading } from "../domain/energy-reading.js";
 import { findAllEnergyReadings } from "../repositories/energy.repository.js";
 
-export function getEnergyReadings() {
+export function getEnergyReadings(): EnergyReading[] {
   return findAllEnergyReadings();
 }
